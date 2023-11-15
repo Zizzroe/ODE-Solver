74f89c13235736fdffff1af476a160299eacb731
Zizzroe <145933300+Zizzroe@users.noreply.github.com>
2023-11-15 12:39:39 -0500

    Update ODE solver.cpp

--- ODE solver/ODE solver.cpp
+++ ODE solver/ODE solver.cpp
@@ -145,6 +145,31 @@ std::string getUserInput() {
     return equation;
 }
 
+float getStep(float x0, float xn, int n) {
+    float stepSize = (xn - x0) / n;
+    return stepSize;
+}
+
+int runge_kutta(std::string equation, float x0, float y0, float xn, float n) {
+    #define f(x,y) equation;
+    float yn, k1, k2, k3, k4, k;
+    int i;
+
+    float h = getStep(x0, xn, n);
+    
+    for (i = 0, i < n; i++) {
+        k1 = h * (f(x0, y0));
+        k2 = h * (f((x0 + h / 2), (y0 + k1 / 2);
+        k3 = h * (f((x0 + h / 2), (y0 + k2 / 2);
+        k4 = h * (f((x0 + h), (y0 + k3);
+        k = (k1 + 2 * k2 + 2 * k3 * k4) / 6;
+        yn = y0 + k;
+        x0 = x0 + h;
+        y0 = yn;
+    }
+    return yn, xn;
+}
+
 int main() {
     std::string infix = getUserInput();
     std::queue<std::string> postfix = postfixEval(infix);
