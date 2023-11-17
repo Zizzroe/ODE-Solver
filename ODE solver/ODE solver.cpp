5c3dc25ce087ae1513f25abe26fb0a93eafb33a9
Zizzroe <145933300+Zizzroe@users.noreply.github.com>
2023-11-17 00:57:57 -0500

    Update ODE solver.cpp

--- ODE solver/ODE solver.cpp
+++ ODE solver/ODE solver.cpp
@@ -11,9 +11,13 @@
 #include <vector>
 #include <stdlib.h>
 #include <iomanip>
-#include <tuple>
+#include <fstream>
 
 #define SIZE 10 
+
+using EquationFunction = std::function<double(float, float)>;
+using EquationSystem = std::function<std::vector<double>(double, const std::vector<double>&)>;
+
 
 void gaussJordanElimination(int n, float coefficients[SIZE][SIZE + 1], float solutions[SIZE]) {
     float ratio;
@@ -40,12 +44,9 @@ void gaussJordanElimination(int n, float coefficients[SIZE][SIZE + 1], float sol
     }
 }
 
-
-using EquationFunction = std::function<double(float, float)>;
-
 EquationFunction getEquation() {
     return[](float x, float y) {
-        return (y * y - x * x ) / ( y * y + x * x ); //TYPE IN YOUR EQUATION HERE
+        return (y * y - x * x) / (y * y + x * x); //TYPE IN YOUR EQUATION HERE
         };
 }
 
@@ -55,7 +56,7 @@ float getStep(float x0, float xn, int n) {
 }
 
 float trapezoidal_integration(float lowerBound, float upperBound, float interval) { //RECOMMENDED VALUE FOR INTERVAL IS MINIMUM OF 500
-#define integrand(x) pow(x, x) //TYPE IN YOUR INTEGRAND HERE
+#define integrand(x) 1 / (1 + pow(x, 2)) //TYPE IN YOUR INTEGRAND HERE
     float integration = 0.0;
     float stepSize, k;
 
@@ -72,32 +73,190 @@ float trapezoidal_integration(float lowerBound, float upperBound, float interval
     return integration;
 }
 
-std::vector<std::tuple<double, double, double>> runge_kutta(const EquationFunction& function, float x0, float y0, float xn, float n) {
-    double yn;
+std::vector<std::pair<double, double>> runge_kutta(const EquationFunction& function, float x0, float y0, float xn, float n) {
     if (n <= 0) {
         std::cerr << "Error: Number of steps should be greater than 0.\n";
         return {};
     }
 
     double h = getStep(x0, xn, n);
-    std::vector<std::tuple<double, double, double>> results;
+    std::vector<std::pair<double, double>> results;
     results.reserve(n + 1); // Reserve space for efficiency
 
     for (int step = 0; step <= n; ++step) {
+        results.emplace_back(x0, y0);
 
         double k1 = h * function(x0, y0);
         double k2 = h * function(x0 + h / 2, y0 + k1 / 2);
         double k3 = h * function(x0 + h / 2, y0 + k2 / 2);
         double k4 = h * function(x0 + h, y0 + k3);
-        double k = (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-        yn = y0 + k;
+
+        y0 = y0 + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
         x0 = x0 + h;
-        y0 = yn;
-        
-        results.emplace_back(x0, y0, yn);
     }
 
     return results;
+}
+
+std::vector<std::pair<double, std::vector<double>>> nsv_euler(const EquationSystem& system, double t0, const std::vector<double>& y0, double tn, int n) {
+    if (n <= 0) {
+        std::cerr << "Error: Number of steps should be greater than 0.\n";
+        return {};
+    }
+
+    double h = (tn - t0) / n;
+    std::vector<std::pair<double, std::vector<double>>> results;
+    results.reserve(n + 1); // Reserve space for efficiency
+
+    std::vector<double> y = y0;
+
+    for (int step = 0; step <= n; ++step) {
+        results.emplace_back(t0 + step * h, y);
+
+        std::vector<double> dy = system(t0 + step * h, y);
+
+        for (size_t i = 0; i < y.size(); ++i) {
+            y[i] = y[i] + h * dy[i];
+        }
+    }
+
+    return results;
+}
+
+void cashKarpMethod(double t, std::vector<double>& y, double& h, const EquationSystem& system, double tolerance) {
+    const double a2 = 1.0 / 5.0;
+    const double a3 = 3.0 / 10.0;
+    const double a4 = 3.0 / 5.0;
+    const double a5 = 1.0;
+    const double a6 = 7.0 / 8.0;
+
+    const double b21 = 1.0 / 5.0;
+
+    const double b31 = 3.0 / 40.0;
+    const double b32 = 9.0 / 40.0;
+
+    const double b41 = 3.0 / 10.0;
+    const double b42 = -9.0 / 10.0;
+    const double b43 = 6.0 / 5.0;
+
+    const double b51 = -11.0 / 54.0;
+    const double b52 = 5.0 / 2.0;
+    const double b53 = -70.0 / 27.0;
+    const double b54 = 35.0 / 27.0;
+
+    const double b61 = 1631.0 / 55296.0;
+    const double b62 = 175.0 / 512.0;
+    const double b63 = 575.0 / 13824.0;
+    const double b64 = 44275.0 / 110592.0;
+    const double b65 = 253.0 / 4096.0;
+
+    const double c1 = 37.0 / 378.0;
+    const double c3 = 250.0 / 621.0;
+    const double c4 = 125.0 / 594.0;
+    const double c6 = 512.0 / 1771.0;
+
+    const double dc1 = c1 - 2825.0 / 27648.0;
+    const double dc3 = c3 - 18575.0 / 48384.0;
+    const double dc4 = c4 - 13525.0 / 55296.0;
+    const double dc5 = -277.0 / 14336.0;
+    const double dc6 = c6 - 0.25;
+
+    std::vector<double> k1 = system(t, y);
+
+    std::vector<double> y2(y.size());
+    for (size_t i = 0; i < y.size(); ++i) {
+        y2[i] = y[i] + b21 * h * k1[i];
+    }
+
+    std::vector<double> k2 = system(t + a2 * h, y2);
+
+    std::vector<double> y3(y.size());
+    for (size_t i = 0; i < y.size(); ++i) {
+        y3[i] = y[i] + h * (b31 * k1[i] + b32 * k2[i]);
+    }
+
+    std::vector<double> k3 = system(t + a3 * h, y3);
+
+    std::vector<double> y4(y.size());
+    for (size_t i = 0; i < y.size(); ++i) {
+        y4[i] = y[i] + h * (b41 * k1[i] + b42 * k2[i] + b43 * k3[i]);
+    }
+
+    std::vector<double> k4 = system(t + a4 * h, y4);
+
+    std::vector<double> y5(y.size());
+    for (size_t i = 0; i < y.size(); ++i) {
+        y5[i] = y[i] + h * (b51 * k1[i] + b52 * k2[i] + b53 * k3[i] + b54 * k4[i]);
+    }
+
+    std::vector<double> k5 = system(t + a5 * h, y5);
+
+    std::vector<double> y6(y.size());
+    for (size_t i = 0; i < y.size(); ++i) {
+        y6[i] = y[i] + h * (b61 * k1[i] + b62 * k2[i] + b63 * k3[i] + b64 * k4[i] + b65 * k5[i]);
+    }
+
+    std::vector<double> k6 = system(t + a6 * h, y6);
+
+    // Calculate the error
+    double error = 0.0;
+    for (size_t i = 0; i < y.size(); ++i) {
+        double delta = c1 * k1[i] + c3 * k3[i] + c4 * k4[i] + c6 * k6[i];
+        error += delta * delta;
+    }
+    error = std::sqrt(error / y.size());
+
+    // Adjust the step size
+    double factor = 0.9 * std::pow(tolerance / error, 0.2);
+    h *= factor;
+
+    // Update the solution
+    if (error < tolerance) {
+        for (size_t i = 0; i < y.size(); ++i) {
+            y[i] = y[i] + dc1 * k1[i] + dc3 * k3[i] + dc4 * k4[i] + dc5 * k5[i] + dc6 * k6[i];
+        }
+    }
+}
+
+std::vector<std::pair<double, std::vector<double>>> nsv_cash_karp(const EquationSystem& system, double t0, const std::vector<double>& y0, double tn, double tolerance) {
+    const int maxSteps = 10000; // Maximum number of steps to avoid infinite loop
+
+    double h = 100000; // Initial step size
+    double minH = 1e-6; // Minimum step size
+    double t = t0;
+    std::vector<double> y = y0;
+
+    std::vector<std::pair<double, std::vector<double>>> results;
+    results.emplace_back(t, y);
+
+    for (int step = 0; t < tn && step < maxSteps; ++step) {
+        cashKarpMethod(t, y, h, system, tolerance);
+
+        // Ensure that h is not too small
+        h = std::max(h, minH);
+
+        t += h;
+        results.emplace_back(t, y);
+    }
+
+    return results;
+}
+
+
+std::vector<double> getLorenzSystem(double t, const std::vector<double>& state) {
+    const double sigma = 10.0;
+    const double rho = 28.0;
+    const double beta = 8.0 / 3.0;
+
+    double x = state[0];
+    double y = state[1];
+    double z = state[2];
+
+    double dxdt = sigma * (y - x);
+    double dydt = x * (rho - z) - y;
+    double dzdt = x * y - beta * z;
+
+    return { dxdt, dydt, dzdt };
 }
 
 
@@ -105,9 +264,13 @@ int main() {
 
     //CASE DETERMINANT
     int EvalType;
+    //SHARED VARIABLES
+    int n; //SLE & STRANGE ATTRACTORS
     //RK4
     EquationFunction function;
-    std::vector<std::tuple<double, double, double>> resultRK4;
+    std::vector<std::pair<double, double>> resultRK4;
+    //RKCK5
+    double tolerance;
     //TRAPEZOIDAL_INTEGRATION
     float resultIntegral;
     double t;
@@ -115,7 +278,10 @@ int main() {
     std::string equation;
     //SLE
     float coefficients[SIZE][SIZE + 1], solutions[SIZE];
-    int n;
+    //STRANGE ATTRACTORS;
+    double t0, tn;
+    std::vector<double> initialState;
+    std::vector<std::pair<double, std::vector<double>>> results;
 
     std::cout << "\n**********ODE SOLVER********** \n";
     std::cout << "1. Evaluate Using 4th Order Runge-Kutta Method \n";
@@ -141,7 +307,7 @@ int main() {
         resultRK4 = runge_kutta(function, x0, y0, xn, n);
         std::cout << "Results for Runge-Kutta method:\n";
         for (const auto& point : resultRK4) {
-            std::cout << "x: " << std::get<0>(point) << ", y: " << std::get<1>(point) << " yn: " << std::get<2>(point) <<"\n";
+            std::cout << "x: " << point.first << ", y: " << point.second << "\n";
         }
         break;
     case 2:
@@ -153,7 +319,7 @@ int main() {
         std::cout << "Enter Number Of Sub Intervals: \n";
         std::cin >> interval;
         resultIntegral = trapezoidal_integration(lowerBound, upperBound, interval);
-        std::cout << "Integral " << equation << "From " << lowerBound << " To " << upperBound << " Is: " << std::setprecision(15) << resultIntegral << "\n";
+        std::cout << "Integral " << equation << "From " << lowerBound << " To " << upperBound << " Is: " << resultIntegral << "\n";
         break;
     case 3:
         std::cout << "Enter The # Of Unknown Variables In Your System Of Linear Equations: \n";
@@ -171,6 +337,47 @@ int main() {
             std::cout << "x[" << i << "] = " << solutions[i] << std::endl;
         }
         break;
+    case 4:
+        std::cout << "Case 4 is just that you already defined some other stuff in the code because you are code literate \n";
+        std::cout << "And In This Case Its Just Some Strange Attractors \n";
+
+        t0 = 0.0; // initial time;
+        tn = 25.0; // run-to time;
+        n = 5000;
+        initialState = { 0.0, 0.1, 0.0 }; // initial conditions
+        results = nsv_euler(getLorenzSystem, t0, initialState, tn, n);
+
+        // Print the results
+        for (const auto& point : results) {
+            std::cout << "t: " << point.first << ", x: " << point.second[0] << ", y: " << point.second[1] << ", z: " << point.second[2] << "\n";
+        }
+
+        // Ask the user if they want to save the output
+        char saveOutput;
+        std::cout << "Do you want to save the output to a file? (y/n): ";
+        std::cin >> saveOutput;
+
+        if (saveOutput == 'y' || saveOutput == 'Y') {
+            // Open a file for writing
+            std::ofstream outputFile("output.txt");
+
+            // Check if the file is successfully opened
+            if (outputFile.is_open()) {
+                // Write the results to the file
+                for (const auto& point : results) {
+                    outputFile << "x: " << point.second[0] << " y : " << point.second[1] << " z : " << point.second[2] << "\n";
+                }
+
+                // Close the file
+                outputFile.close();
+                std::cout << "Results written to output.txt\n";
+            }
+            else {
+                std::cerr << "Error opening output.txt for writing\n";
+            }
+        }
+        break;
+
     default:
         std::cerr << "Invalid option\n";
         break;
